e01fe9a831d55b70c435641f65e8bc5684cd4e8f
dkantikorn <dk_antikorn@hotmail.com>
2017-09-20 15:22:48 +0700

    add book data

--- src/app/book-data.ts
+++ src/app/book-data.ts
@@ -3,13 +3,13 @@ import { InMemoryDbService } from 'angular2-in-memory-web-api';
 export class BookData implements InMemoryDbService {
   createDb() {
     let books = [
-      { id: '1', name: 'Angular 2 by Krishna', category: 'Angular', writer: 'Krishna' },
-      { id: '2', name: 'AngularJS by Krishna', category: 'Angular', writer: 'Krishna' },
-      { id: '3', name: 'Angular 2 by Vishnu', category: 'Angular', writer: 'Vishnu' },	
-      { id: '4', name: 'Core Java by Vishnu', category: 'Java', writer: 'Vishnu' },
-      { id: '5', name: 'JSP & Servlet by Vishnu', category: 'Java', writer: 'Vishnu' },
-      { id: '6', name: 'JPA by Vishnu', category: 'Java', writer: 'Vishnu' },
-      { id: '7', name: 'Hibernate by Krishna', category: 'Hibernate', writer: 'Krishna' }
+      { id: '1', name: 'Angular 2 with Angular-CLI', category: 'Angular', writer: 'sarawutt.b' },
+      { id: '2', name: 'AngularJS simple Application on CRUD', category: 'Angular', writer: 'sarawutt.b' },
+      { id: '3', name: 'Angular 2 with Typescript', category: 'Angular', writer: 'sarawutt.b' },	
+      { id: '4', name: 'Core Java OOP', category: 'Java', writer: 'sarawutt.b' },
+      { id: '5', name: 'JSP & Servlet', category: 'Java', writer: 'sarawutt.b' },
+      { id: '6', name: 'JPA', category: 'Java', writer: 'sarawutt.b' },
+      { id: '7', name: 'Hibernate', category: 'Hibernate', writer: 'sarawutt.b' }
     ];
     return {books};
   }
